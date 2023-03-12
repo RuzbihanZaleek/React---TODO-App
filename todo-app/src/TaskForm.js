2607e8ef0b0d132999cb7c7151c71f3adb7ff0e2
RuzbihanZaleek <ruzbihan.z@eyepax.com>
2023-03-12 20:02:33 +0530

    inital commit

---
+++ todo-app/src/TaskForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TaskForm(props) {
+    return (
+        <form>
+            <button>+</button>
+            <input typer="text" />
+        </form>
+    );
+}
+
+export default TaskForm;
